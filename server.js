ee670de65df0c4e8bd9a6aa86a7cd30d5453e409
Suman Kumar <139070977+suman987654432@users.noreply.github.com>
2026-01-08 16:29:45 +0530

    Add MongoDB and Stripe environment variables
    Added environment variables for MongoDB and Stripe configuration.

--- server.js
+++ server.js
@@ -21,5 +21,9 @@ app.use("/students", userRouter)
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
 })
+MONGOURI=mongodb+srv://sumanqaj9876_db_user:NZfmbKlOSPm1VBfB@cluster0.ykwsx1q.mongodb.net/?appName=Cluster0
+STRIPE_SECRET_KEY=sk_test_51SnCbAHFeLQhzbfNGjdwhVmrE77gPrJB5C31bIp46EC1cIOHDRAq5zNyrYTgb6KdAlHuocuHg4FU8v0Uyd6pZaUO00irhh7J3B
+STRIPE_WEBHOOK_SECRET=whsec_your_webhook_secret_here
+STRIPE_PUBLISHABLE_KEY=pk_test_51SnCbAHFeLQhzbfN7N9XWfuigFJmIEFUKlehJlFr1wBlKOK1fvVWHNQb4chTsQOtIW5Js3bCueIwOIO0W0Sz87SE00f9UmrrU2
 
 
